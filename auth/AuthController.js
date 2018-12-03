4c47e4754bf8f163b572ab42ee55c8fce73e31d8
Unknown <diefrodriguezcha@unal.edu.co>
2018-12-03 00:21:29 -0500

    More changes

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -30,24 +30,24 @@ router.post('/register', (req, res) => {
         email: req.body.email,
         password: hashedPassword
     }, (err, user) => {
-        if (err) return res.status(500).send({auth: false, message: "Hubo un problema con el registro de usuario."});
+        if (err) return res.send({auth: false, message: "Hubo un problema con el registro de usuario."});
 
         // creacion token
         let token = jwt.sign({id: user._id}, config.secret, {
             expiresIn: 86400 //24 horas = 1 dia
         });
         console.log('/register')        
-        res.status(200).send({auth: true, token: token, expiresIn: 86400});
+        res.send({auth: true, token: token, expiresIn: 86400});
     })
 });
 
 router.get('/me', verifyToken, (req, res, next) => {
     console.log("/me");
     User.findById(req.userId, { password: 0 }, (err, user) => {
-        if (err) return res.status(404).send({ auth: false, message: "Hubo un problema al buscar el usuario."});
-        if (!user) return res.status(404).send({ auth: false, message: "No se encontro el usuario."});
+        if (err) return res.send({ auth: false, message: "Hubo un problema al buscar el usuario."});
+        if (!user) return res.send({ auth: false, message: "No se encontro el usuario."});
         
-        res.status(200).send(user);
+        res.send(user);
     });
 });
 
@@ -62,8 +62,8 @@ const connect = () => {
 router.post("/login", (req, res) => {
     console.log("/login");
     User.findOne({ email: req.body.email }, (err, user) => {
-        if (err) return res.status(500).send({ auth: false, message: "Error en servidor al autenticar."});
-        if (!user) return res.status(404).send({ auth: false, message: "El usuario no se encontro en la base de datos."});
+        if (err) return res.send({ auth: false, message: "Error en servidor al autenticar."});
+        if (!user) return res.send({ auth: false, message: "El usuario no se encontro en la base de datos."});
 
         let clientldap = connect();
         clientldap.on('connect', () => {
@@ -72,15 +72,15 @@ router.post("/login", (req, res) => {
             let password = req.body.password;
             let dn = "cn=" + username + ",ou=apollo,dc=apollo,dc=unal,dc=edu,dc=co";
             clientldap.bind(dn, password, (err) => {
-                if (err) return res.status(500).send({ auth: false, message: 'El usuario no existe en el servidor LDAP.'});
+                if (err) return res.send({ auth: false, message: 'El usuario no existe en el servidor LDAP.'});
                 
                 let passwordIsInvalid = bcrypt.compareSync(req.body.password, user.password);
-                if (!passwordIsInvalid) return res.status(401).send({ auth: false, message: "La contraseña no es la correcta." });
+                if (!passwordIsInvalid) return res.send({ auth: false, message: "La contraseña no es la correcta." });
 
                 let token = jwt.sign({ id: user._id }, config.secret, {
                     expiresIn: 86400// Expira en 24 horas
                 });
-                res.status(200).send({ auth: true, token: token, expiresIn: 86400 });                        
+                res.send({ auth: true, token: token, expiresIn: 86400 });                        
             });
         });
     });
--- auth/VerifyToken.js
+++ auth/VerifyToken.js
@@ -3,10 +3,10 @@ const config = require("../config");
 
 const verifyToken = (req, res, next) => {
     let token = req.headers['x-access-token'];
-    if (!token) return res.status(403).send({ auth: false, message: 'Sin token no hay acceso.' });
+    if (!token) return res.send({ auth: false, message: 'Sin token no hay acceso.' });
 
     jwt.verify(token, config.secret, (err, decoded) => {
-        if (err) return res.status(500).send({ auth: false, message: 'Falla al autenticar el token.' });
+        if (err) return res.send({ auth: false, message: 'Falla al autenticar el token.' });
 
         req.userId = decoded.id;
         next();
