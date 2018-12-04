0231499843b2c16eef97bb8fbafb7bab117d4b04
Diego Rodriguez <diefrodriguezcha@unal.edu.co>
2018-12-04 01:58:26 -0500

    Changed for proxy

--- connection_mongo.js
+++ connection_mongo.js
@@ -11,8 +11,9 @@ const options = {
 
 const startingMongoDB = () => {
     const adress = process.env.DOCKER_DEPLOY? 'sa-user-db': '127.0.0.1';    
-    const database = process.env.DATABASE? '27018':'27017';
+    const database = process.env.DATABASE? process.env.DATABASE:'27017';
     mongoose.connect(`mongodb://${adress}:${database}/users`, options, (err) => {
+        console.log(`mongodb://${adress}:${database}/users`);
         if (err) {
             console.log('Not connected to users');
             setTimeout(startingMongoDB, 5000);
