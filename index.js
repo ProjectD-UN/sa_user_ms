45b95497885d7a9d92b7325e5bf372e8af568398
Diego Rodriguez <diefrodriguezcha@unal.edu.co>
2018-12-05 23:42:27 -0500

    Changed 0.0.0.0

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ const port =  process.env.PORT || 3003;
 app.use('/api/auth', AuthController);
 app.use('/api/auth', LDAP);
 DBConnection();
-app.listen(port, () => console.log(`sa_user_ms started! Listening on ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`sa_user_ms started! Listening on ${port}`));
